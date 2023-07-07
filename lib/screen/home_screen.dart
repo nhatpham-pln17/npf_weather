50e0cdda8fc28fa0e1a435cfaff0c787f7c8e016
nhatpham <nhatpham.pln17@gmail.com>
2023-07-07 18:12:42 +0700

    Add Header widget

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:npf_weather/controller/global_controller.dart';
+import 'package:npf_weather/widget/header_widget.dart';
+
+class HomeScreen extends StatefulWidget{
+  const HomeScreen({Key? key}): super(key:key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>{
+
+  //call
+   final GlobalController globalController =
+      Get.put(GlobalController(), permanent: true);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: Obx(() => globalController.checkLoading().isTrue
+      ? const Center(child: CircularProgressIndicator(),)
+      :ListView(
+        scrollDirection: Axis.vertical,
+        children: [
+          const SizedBox(height: 10),
+          const HeaderWidget(),
+        ],
+      ),)),
+    );
+  }
+}
